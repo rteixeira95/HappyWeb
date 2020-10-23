c2bb80be5b4e4e0e5185e8f9b47da773f8fcb64c
Rui Teixera <rui.c.teixeira.95@gmail.com>
2020-10-23 12:55:19 +0000

    finish second video

--- public/scripts/page-orphanage.js
+++ public/scripts/page-orphanage.js
@@ -1,6 +1,14 @@
+const options = {
+    draggin: false,
+    touchZoom: false,
+    doubleClickZoom: false,
+    scrollwheelZoom: false,
+    zoomControl: false
+}
+
 //create map 
 const map = L
-    .map("mapid")
+    .map("mapid", options)
     .setView([-27.222633, -49.6455874], 15);
 //create and add titleLayer
 L
@@ -15,16 +23,31 @@ const icon = L
         popupAnchor: [170, 2]
 
     });
-//create popup layer
-const popup = L.popup({
-    closeButton: false,
-    className: "map-popup",
-    minWidth: 240,
-    minHeight: 240
-}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg" > </a>')
+
 //create and add marker
 
 L
-    .marker([-27.222633, -49.6455874],{icon})
-    .addTo(map)
-    .bindPopup(popup);
+    .marker([-27.222633, -49.6455874], { icon })
+    .addTo(map);
+
+/* image gallery */
+
+function selectImage(event) {
+    const button = event.currentTarget
+    //remover todas as class active
+    const buttons = document.querySelectorAll(".images button")
+    buttons.forEach(removeActiveClass)
+    function removeActiveClass(button) {   
+        button.classList.remove("active")
+    }
+
+    //selecionar a imagem clicada
+    const image = button.children[0]
+    const imageContainer = document.querySelector(".orphanage-details > img")
+
+    //atualizar o container da imagem 
+    imageContainer.src = image.src
+
+    //adicionar a class active para o botão 
+    button.classList.add("active")
+}
